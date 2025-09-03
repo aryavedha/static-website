e17752b694d7a9a20fe722b5320285da126d5167
arya vedha <aryavedha@gmail.com>
2025-09-03 13:39:09 +0530

    css, js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('mobile-menu-button').addEventListener('click', function() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    if (mobileMenu.classList.contains('hidden')) {
+        mobileMenu.classList.remove('hidden');
+    } else {
+        mobileMenu.classList.add('hidden');
+    }
+});
+
